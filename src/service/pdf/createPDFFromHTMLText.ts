88b66c44bb24c002a5bfd21f60d0dfb4a917e1da
manalab17 <manalab17@gmail.com>
2023-12-25 17:52:36 +0900

    Func: Add createPDFFromHTMLText service

---
+++ src/service/pdf/createPDFFromHTMLText.ts
@@ -0,0 +1,46 @@
+import {Browser} from "puppeteer-core";
+
+const puppeteer = require("puppeteer-core");
+
+const chromium = require("@sparticuz/chromium");
+chromium.setHeadlessMode = true;
+
+export async function createPDFFromHTMLText(htmlText: string): Promise<Buffer> {
+    let browser: Browser | null = null;
+
+    try {
+        const launchParams = {
+            args: chromium.args,
+            executablePath: await chromium.executablePath(),
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
+        };
+        browser = await puppeteer.launch(launchParams) as Browser;
+
+        const page = await browser.newPage();
+
+        await page.setContent(htmlText);
+
+        const pdfFileBuffer = await page.pdf({
+            format: 'a4',
+            printBackground: true,
+            margin: {
+                left: '0px',
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+            }
+        });
+
+        return pdfFileBuffer;
+    } catch (e) {
+        console.error('Failed to createPDFFromHTML : ', e.message);
+        throw new Error();
+    } finally {
+        if (browser) {
+            console.log('Close browser!');
+            await browser.close();
+        }
+    }
+}
