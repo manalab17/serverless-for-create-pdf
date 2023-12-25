4ce980c8cba301e7263dafaf85f42b71fec5b6a9
manalab17 <manalab17@gmail.com>
2023-12-25 20:18:08 +0900

    Func: Add createPDFWithUpload serverless function

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import {APIGatewayEvent, Context} from "aws-lambda";
 import {getDefaultResponse} from "./src/common/response";
 import {createPDF} from "./src/api/createPDF";
+import {createPDFWithUpload} from "./src/api/createPDFWithUpload";
 
 async function createPDFHandler(event: APIGatewayEvent & { isWarmer?: boolean }, ctx: Context) {
     ctx.callbackWaitsForEmptyEventLoop = false;
@@ -15,6 +16,20 @@ async function createPDFHandler(event: APIGatewayEvent & { isWarmer?: boolean },
     return result;
 }
 
+async function createPDFWithUploadHandler(event: APIGatewayEvent & { isWarmer?: boolean }, ctx: Context) {
+    ctx.callbackWaitsForEmptyEventLoop = false;
+    const { isWarmer = false } = event;
+
+    if (isWarmer) {
+        return getDefaultResponse();
+    }
+
+    const result = await createPDFWithUpload(event);
+
+    return result;
+}
+
 export {
-    createPDFHandler
+    createPDFHandler,
+    createPDFWithUploadHandler
 };
